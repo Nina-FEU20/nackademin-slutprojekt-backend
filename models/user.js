d4cc9109e640075d8d81b88dd4f03566af9e089c
Nina Jivetorp <ninajivetorp@gmail.com>
2021-05-18 14:05:58 +0200

    minor errors fixed

--- models/user.js
+++ models/user.js
@@ -2,38 +2,41 @@ const mongoose = require('mongoose')
 const { Schema } = mongoose
 
 const userSchema = new Schema({
-    
-        _id:{ type : mongoose.Schema.Type.ObjectId, required : true},
-        email: {
-            type : String, 
-            required : true
-            },
-        password: {
-            type : String, 
-            required : true
-            },
-        name: { 
-            type : String, 
-            required : true
-            },
-        role: String,
-    
-        adress: {
-            street: { 
-                type : String, 
-                required : true
-            },
-            zip: { 
-                type : Number, 
-                required : true
-            },
-            city: { 
-                type : String, 
-                required : true
-            }
+
+    _id: { type: mongoose.Schema.Types.ObjectId, required: true },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    role: String,
+
+    adress: {
+        street: {
+            type: String,
+            required: true
         },
-        orderHistory: [...String ]
-    })
+        zip: {
+            type: Number,
+            required: true
+        },
+        city: {
+            type: String,
+            required: true
+        }
+    },
+    orderHistory: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Order'
+    }]
+})
 
 
-modules.export = mongoose.model('User', userSchema)
+module.export = mongoose.model('User', userSchema)
