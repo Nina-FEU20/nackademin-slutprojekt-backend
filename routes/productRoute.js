385c99382e35b8e9cb78ce275247f428cb3a7ba5
Frida Waldt <fridawaldt@gmail.com>
2021-05-18 18:43:15 +0200

    Get all Products + Specific Product
    Ordnat get till alla produkter och specifika produkter

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -18,13 +18,27 @@ router.post('/', (req,res) => {
             console.error(err)
         } else{
             console.log('The new product has been saved')
+            res.json('The new product has been saved')
         }
     })
 })
 
 
+router.get('/', async (req,res) => {
+    const allProducts = await Product.find({})
+
+    res.json(allProducts)
+})
+
+router.get('/:id', async (req,res) => {
+    const specProduct = await Product.find({_id: req.params.id})
+
+    res.json(specProduct)
+})
+
 
 module.exports = router
+
 
 /* 
 const products = [
