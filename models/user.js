031b6ca05aaddc9b1632411cb7458ad72f63b79a
SaraWallener <69631195+SaraWallener@users.noreply.github.com>
2021-05-19 10:13:48 +0200

    update user.js
    Added more in user.js

--- models/user.js
+++ models/user.js
@@ -2,33 +2,41 @@ const mongoose = require('mongoose')
 
 const userSchema = mongoose.Schema({
 
-    _id: { type: mongoose.Schema.Types.ObjectId, required: true },
+    _id: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        required: true 
+    },
     email: {
         type: String,
-        required: true
+        required: [true, 'Email is required'],
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
     },
     password: {
         type: String,
-        required: true
+        required: [true, 'Password is required'],
+        min: [8, 'Password require minimum of 8 characters'],
+
     },
     name: {
         type: String,
-        required: true
+        required: [true, 'Name is required'],
+        match: [/^[a-zA-Z]+$/, 'Only letters allowed']
     },
     role: String,
 
     adress: {
         street: {
             type: String,
-            required: true
+            required: [true, 'Street is required']
         },
         zip: {
             type: Number,
-            required: true
+            required: [true, 'Zip is required'],
+            match: [/^[0-9]+$/, 'Only numbers allowed']
         },
         city: {
             type: String,
-            required: true
+            required: [true, 'City is required']
         }
     },
     orderHistory: [{
