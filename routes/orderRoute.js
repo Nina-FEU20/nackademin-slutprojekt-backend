bf91f00de73d0fbdfce7d0ec6e860036ef3ba5df
Nina Jivetorp <ninajivetorp@gmail.com>
2021-05-27 12:27:29 +0200

    Fixed orderValue
    + some errorhandling

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -4,21 +4,13 @@ const Order = require('../models/order');
 const Product = require('../models/product');
 const { verifyIsLoggedIn } = require('../authentication')
 
-
 router.post('/', verifyIsLoggedIn, async (req, res) => {
 
     // getting the items in the cart
     let items = req.body.items;
 
     // if statement for the cart, if its nothing inside === you can not submit order.
-    if(items === null || items === undefined) {
-        res.status(404).send('no items in cart');
-        // it ends here.
-        return;
-    }
-
-    // if items is equal to 0, which also means you can not submit order.
-    if (items.length === 0) {
+    if (!items || items.length === 0) {
         res.status(404).send('no items in cart');
         // it ends here.
         return;
@@ -27,32 +19,43 @@ router.post('/', verifyIsLoggedIn, async (req, res) => {
     // getting the logged in user using the req.verifiedUser that we get from the middleware verifyIsLoggedIn 
     const user = await User.findOne({ _id: req.verifiedUser.id });
 
+    try {
 
-    // it will check through all the products and match the correct id for product you wish to buy.
-    const products = await Product.find({ _id: { $in: items } });
+        // creating an empty array to push all itemprices in to 
+        const products = [];
+        for (let item of items) {
+            // getting each product
+            const product = await Product.find({ _id: item })
+            // pushing every price into the array
+            products.push(product[0].price)
+        }
 
-
-    // creating order from order models.
-    let order = new Order({
-        timeStamp: Date.now(),
-        status: 'inProcess',
-        items: items,
         // reduce looping through products, then it will return the total value of the price for the products in array, 0 = it begins from zero price.
-        orderValue: products.reduce((total, prod) => total + prod.price, 0)
-    });
+        let total = products.reduce((total, currentProduct) => {
+            return total + currentProduct
+        }, 0)
 
+        // creating order from order models.
+        let order = new Order({
+            timeStamp: Date.now(),
+            status: 'inProcess',
+            items: items,
+            orderValue: total
+        });
 
-    // Saving the order to database
-    await order.save();
+        // Saving the order to database
+        await order.save();
 
-    // orderHistory is inside user, so the order._id gets pushed into user. (if we are logged in)
-    if (user) {
+        // orderHistory is inside user, so the order._id gets pushed into user.
         user.orderHistory.push(order._id);
-        // it saves when customer orders items, whatever he order, updates to his cart. Only for the specific logged in user. Because of the ID.
-        await User.findByIdAndUpdate(user._id, user);
-    }
+        // saving the user again with the updated orderhistory
+        user.save()
 
-    res.status(200).json(order);
+        res.status(200).json(order);
+
+    } catch (err) {
+        return res.send(err.message)
+    }
 });
 
 
@@ -67,8 +70,8 @@ router.get('/', verifyIsLoggedIn, async (req, res) => {
         res.json(orders);
         // If it is customer you will see your order.
     } else if (user.role === 'customer') {
-        const user = await User.findOne({ _id: req.verifiedUser.id},
-             // if there is 1 in orderHistory it will connect, if its 0, it will not. populate calls the orderHistory field which references to the ObjectId in the document.
+        const user = await User.findOne({ _id: req.verifiedUser.id },
+            // if there is 1 in orderHistory it will connect, if its 0, it will not. populate calls the orderHistory field which references to the ObjectId in the document.
             { orderHistory: 1 }).populate('orderHistory');
         res.json(user.orderHistory);
     }
